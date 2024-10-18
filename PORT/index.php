c2be41fc404c6f25a2e399665e2237bf1c16bef2
dgf <96919401+StultusEstQuiHocLegit@users.noreply.github.com>
2024-10-18 15:26:27 +0200

    Add files via upload

--- PORT/index.php
+++ PORT/index.php
@@ -27,10 +27,50 @@ if (!$isLoggedIn) {
     // User is logged in, check the role from cookie
     if ($userRole === 0) {
         // Explorer
-        include("explore.php");
+        if ($content === 'explore.php') {
+            include("explore.php");
+        } elseif ($content === 'cart.php') {
+            include("cart.php");
+        } elseif ($content === 'PreviousCarts.php') {
+            include("PreviousCarts.php");
+        } elseif ($content === 'account.php') {
+            include("account.php");
+        } else {
+            // Default landing page for explorers
+            include("explore.php");
+        }
     } elseif ($userRole === 1) {
         // Creator
-        include("dashboard.php");
+        if ($content === 'dashboard.php') {
+            include("dashboard.php");
+        } elseif ($content === 'orders.php') {
+            include("orders.php");
+        } elseif ($content === 'inventory.php') {
+            include("inventory.php");
+        } elseif ($content === 'products.php') {
+            include("products.php");
+        } elseif ($content === 'ExplorersCustomers.php') {
+            include("ExplorersCustomers.php");
+        } elseif ($content === 'CreatorsSuppliers.php') {
+            include("CreatorsSuppliers.php");
+        } elseif ($content === 'explore.php') {
+            include("explore.php");
+        } elseif ($content === 'cart.php') {
+            include("cart.php");
+        } elseif ($content === 'PreviousCarts.php') {
+            include("PreviousCarts.php");
+        } elseif ($content === 'accounting.php') {
+            include("accounting.php");
+        } elseif ($content === 'help.php') {
+            include("help.php");
+        } elseif ($content === 'account.php') {
+            include("account.php");
+        } elseif ($content === 'YourWebsite.php') {
+            include("YourWebsite.php");
+        } else {
+            // Default landing page for creators
+            include("dashboard.php");
+        }
     }
 }
 echo "<br><br><br><br><br><br><br><br><br><br>";
